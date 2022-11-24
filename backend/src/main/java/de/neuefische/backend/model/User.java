9f8dce2368c48583ed695c60b629c8ba34c8047f
LilianCharlotte <Lilian.krieg@live.de>
2022-11-24 15:54:24 +0100

    Add initial datatypes backend

---
+++ backend/src/main/java/de/neuefische/backend/model/User.java
@@ -0,0 +1,12 @@
+package de.neuefische.backend.model;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public record User(
+        String id,
+        String name,
+        List<Country> countryList
+) {
+}
