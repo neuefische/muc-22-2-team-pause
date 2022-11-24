b35096f5b3d7c53da99b881a0f4c8f9bcea4ec08
LilianCharlotte <Lilian.krieg@live.de>
2022-11-24 15:59:46 +0100

    Add initial datatypes frontend

---
+++ frontend/src/model/Country.ts
@@ -0,0 +1,7 @@
+
+
+export type Country = {
+    name : string,
+    threeLetterCode : string,
+    flag: string
+}
---
+++ frontend/src/model/User.ts
@@ -0,0 +1,13 @@
+import {Country} from "./Country";
+
+export type User = {
+    name : string,
+    id: string,
+    visitedCountries: Country[]
+}
+
+export type NewUser = {
+    name : string,
+    visitedCountries: Country[]
+
+}
