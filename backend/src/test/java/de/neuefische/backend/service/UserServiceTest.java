26f4eb9eae897f0f46773edc03d124036ce9b573
nick <klockgether.nick@gmail.com>
2022-11-25 10:12:23 +0100

    Update UserService constructor params with UuidGenratorService, update Unit Tests with properly mock repos

--- backend/src/test/java/de/neuefische/backend/service/UserServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/service/UserServiceTest.java
@@ -8,10 +8,14 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class UserServiceTest {
 
-    UserService userService = new UserService(new UserRepo(new ArrayList<>()));
+    UserRepo userRepo = mock(UserRepo.class);
+    UuidGeneratorService uuidGeneratorService = mock(UuidGeneratorService.class);
+    UserService userService = new UserService(userRepo,uuidGeneratorService);
+
     @Test
     void listUsers_expect_emptyList() {
         List<User> expList = new ArrayList<>();
@@ -22,16 +26,16 @@ class UserServiceTest {
     }
 
     @Test
-    void addUser_expect_valid_user_not_empty_id() {
+    void addUser_expect_valid_user() {
         //given
         User givenUser = new User(null,"nick",new ArrayList<>());
 
         //when
+        when(userRepo.addUser(givenUser)).thenReturn(givenUser);
         User result = userService.addUser(givenUser);
 
         //then
-        assertInstanceOf(User.class,result);
-        assertFalse(result.id().isEmpty());
-        assertFalse(result.id().isBlank());
+        assertEquals(givenUser,result);
+        verify(userRepo).addUser(givenUser);
     }
 }
