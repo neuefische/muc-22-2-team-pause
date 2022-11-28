7700ab621fdd2b8708999fa710e0083e7acc287c
nick <klockgether.nick@gmail.com>
2022-11-28 16:11:10 +0100

    Move LoggedInUser to App.tsx

--- frontend/src/component/SignUp.tsx
+++ frontend/src/component/SignUp.tsx
@@ -1,18 +1,28 @@
 import AddUser from "./AddUser";
-import {NewUser} from "../model/User";
+import {NewUser, User} from "../model/User";
 import {addUser} from "../apiCalls";
 import {useNavigate} from "react-router-dom";
 
-export default function SignUp (){
+type SignUpProps = {
+    setLoggedInUser(data: User): void;
+}
+
+export default function SignUp(props: SignUpProps) {
     const navigate = useNavigate()
 
-    function createUser(user:NewUser){
-        addUser(user).then(data => {
-            navigate("/overview",{state:data})
-        })
+    function createUser(user: NewUser) {
+        addUser(user)
+            .then(data => {
+                handleNewUser(data)
+                navigate("/overview")
+            })
     }
 
-    return(
+    function handleNewUser(data: User) {
+        props.setLoggedInUser(data)
+    }
+
+    return (
         <AddUser createUser={createUser}/>
     )
 }
