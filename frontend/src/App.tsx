39c4a65838d12869d51307dc02e5db0974de5817
nick <klockgether.nick@gmail.com>
2022-11-28 14:25:20 +0100

    Update ApiCall to return data only, Add UserOverview, Add useUsers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import SignUp from "./component/SignUp";
+import UserOverview from "./component/UserOverview";
 
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<SignUp/>}/>
-                    <Route path="/overview" element={<p>Overview</p>}></Route>
+                    <Route path="/overview" element={<UserOverview/>}></Route>
                     <Route path="/overview/:id" element={<p>detail</p>}></Route>
                     <Route path="/overview/:id/profile" element={<p>edit name</p>}></Route>
                     <Route path="/overview/:id/countries" element={<p>edit countries</p>}></Route>
