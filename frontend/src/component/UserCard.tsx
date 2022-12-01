78b7f741ef319a010d5f0d622019af1c8c7a8ce1
Nick Klockgether <33906240+NickKlock@users.noreply.github.com>
2022-12-01 12:38:59 +0100

    Update frontend/src/component/UserCard.tsx
    Flo didn't like our naming
    
    Co-authored-by: Florian Weber <62606044+Flooooooooooorian@users.noreply.github.com>

--- frontend/src/component/UserCard.tsx
+++ frontend/src/component/UserCard.tsx
@@ -28,8 +28,7 @@ export default function UserCard(props: UserCardProps) {
     return (<div>
             <h2>Name:{props.user.name} </h2>
             <div>{props.user.visitedCountries &&
-                props.user.visitedCountries.map((c) =>
-                {return <p key={c.threeLetterCode}> {c.name}[{c.threeLetterCode}] {c.flag}</p>})}
+                props.user.visitedCountries.map((country) => <p key={country.threeLetterCode}> {country.name}[{country.threeLetterCode}] {country.flag}</p>})
             </div>
             {props.loggedInUser.id === props.user.id && <button onClick={handleDeleteUser}>Delete User</button>}
             {props.loggedInUser.id === props.user.id && <button
