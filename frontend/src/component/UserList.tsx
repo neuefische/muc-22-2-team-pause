9d130790941b6ac8ea3fedd36c08ad849dc41810
fangfangwang <fannie-id@hotmail.com>
2022-12-02 14:09:39 +0100

    move search function from userList to userOverview

--- frontend/src/component/UserList.tsx
+++ frontend/src/component/UserList.tsx
@@ -1,6 +1,5 @@
 import {User} from "../model/User";
 import UserCard from "./UserCard";
-import {ChangeEvent, useState} from "react";
 
 type UserListProps = {
     users: User[]
@@ -11,31 +10,14 @@ type UserListProps = {
 }
 
 export default function UserList(props: UserListProps) {
-    const [searchQuery, setSearchQuery] = useState("")
 
     function handleLogInAs(user: User) {
         props.handleLoginAs(user)
     }
 
-    const filteredUsers = props.users.filter(user => {
-        return user.name.toLowerCase().includes(searchQuery.toLowerCase())
-            || user.visitedCountries.every(country => {
-                return country.name.toLowerCase().includes(searchQuery.toLowerCase())
-                    || country.threeLetterCode.toLowerCase().includes(searchQuery.toLowerCase())
-            })
-    })
-
-    function handleSearchText(event: ChangeEvent<HTMLInputElement>) {
-        setSearchQuery(event.target.value)
-    }
-
     return (
         <div>
-            <div>
-                <input type={"search"} onChange={handleSearchText} placeholder={"Search for user or countries.."}/>
-            </div>
-
-            {filteredUsers.map(user => <UserCard
+            {props.users.map(user => <UserCard
                 key={user.id}
                 user={user}
                 handleLoginAs={handleLogInAs}
