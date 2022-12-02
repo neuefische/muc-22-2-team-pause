64ce0d5302c5c8cc6fcf7aeba19fe58e33f96928
nick <klockgether.nick@gmail.com>
2022-12-02 17:23:55 +0100

    Update Autocomplete, Remov now useless files

--- frontend/src/component/AddVisitCountry.tsx
+++ frontend/src/component/AddVisitCountry.tsx
@@ -1,6 +1,5 @@
 import {Country} from "../model/Country";
-import {ChangeEvent, useState} from "react";
-import CountryCard from "./CountryCard";
+import {SyntheticEvent} from "react";
 import "./AddVisitCountry.css";
 import {Traveller} from "../model/User";
 import {useNavigate} from "react-router-dom";
@@ -12,51 +11,30 @@ type AddVisitCountryProps = {
     loggedInTraveller: Traveller
 }
 export default function AddVisitCountry(props: AddVisitCountryProps) {
-    const [searchQuery, setSearchQuery] = useState("")
     const navigate = useNavigate()
 
-    const filteredCountries = props.countries.filter(country => {
-        return country.name.toLowerCase().includes(searchQuery.toLowerCase())
-            || country.threeLetterCode.toLowerCase().includes(searchQuery.toLowerCase())
-            || country.flag.toLowerCase().includes(searchQuery.toLowerCase())
-    })
+    function addCountryToUser(event: SyntheticEvent<Element, Event>, value: Country | null) {
+        if (!props.loggedInUser.visitedCountries) {
+            props.loggedInUser.visitedCountries = []
+        }
+        if (value != null) {
+            props.loggedInUser.visitedCountries.push(value)
 
-    function handleSearchText(event: ChangeEvent<HTMLInputElement>) {
-        setSearchQuery(event.target.value)
-    }
-
-
-    function addCountryToUser(country: Country) {
-        if (!props.loggedInTraveller.visitedCountries) {
-            props.loggedInTraveller.visitedCountries = []
+            updateUser(props.loggedInUser.id, props.loggedInUser)
+                .then(() => {
+                    navigate("/overview")
+                })
+                .catch(console.error)
         }
 
-        props.loggedInTraveller.visitedCountries.push(country)
-
-        updateUser(props.loggedInTraveller.id, props.loggedInTraveller)
-            .then(() => {
-                navigate("/overview")
-            })
-            .catch(console.error)
     }
 
-    return (
-        <div>
-            <Autocomplete renderInput={(params) =>
+    return (<Autocomplete
+            onChange={addCountryToUser}
+            renderInput={(params) =>
                 <TextField {...params} label={"Choose a country"}/>}
-                          getOptionLabel={(option) => option.name}
-                          options={props.countries}
-            />
-            <div>
-                <input type={"search"} onChange={handleSearchText} placeholder={"Search for a country.."}/>
-            </div>
-            <div className={"card-display"}>
-                {filteredCountries.length < 5 && filteredCountries.map((value) => {
-                    return <CountryCard country={value}
-                                        addCountryToUser={addCountryToUser}
-                                        key={value.threeLetterCode}/>
-                })}
-            </div>
-        </div>
+            getOptionLabel={(option) => option.name}
+            options={props.countries}
+        />
     )
 }
