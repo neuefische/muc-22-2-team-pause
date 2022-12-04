42d932b7560d7f4d7656d4ef923fd47f2f549838
nick <klockgether.nick@gmail.com>
2022-12-04 15:14:39 +0100

    Create Basic NavBar

---
+++ frontend/src/component/NavBar.tsx
@@ -0,0 +1,12 @@
+import {AppBar, Toolbar} from "@mui/material";
+
+export default function NavBar () {
+
+    return(<AppBar position={"sticky"} color={"secondary"} component={"nav"}>
+            <Toolbar>
+
+            </Toolbar>
+        </AppBar>
+
+    )
+}
