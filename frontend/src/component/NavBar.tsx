0ab0118bb78e91187a995d9419ee180128d9febc
nick <klockgether.nick@gmail.com>
2022-12-04 15:31:08 +0100

    Add Title + Buttons to navbar, Add handleNavigation function

--- frontend/src/component/NavBar.tsx
+++ frontend/src/component/NavBar.tsx
@@ -1,10 +1,44 @@
-import {AppBar, Toolbar} from "@mui/material";
+import {AppBar, Box, Button, Toolbar, Typography} from "@mui/material";
+import React from "react";
+import {useNavigate} from "react-router-dom";
 
-export default function NavBar () {
+export default function NavBar() {
+    const navItems = [
+        {
+            name: 'Home',
+            path: "/"
+        }
+        , {
+            name: 'Sign Up',
+            path: "/"
+        }, {
+            name: 'Overview',
+            path: "/overview"
+        }];
 
-    return(<AppBar position={"sticky"} color={"secondary"} component={"nav"}>
+    const navigate = useNavigate();
+
+    function handleNavigation(path: string) {
+        navigate(path)
+    }
+
+    return (<AppBar position={"sticky"}
+                    color={"secondary"}
+                    component={"nav"}
+        >
             <Toolbar>
+                <Typography
+                    variant="h6"
+                    component="div"
+                    sx={{flexGrow: 1, display: 'flex'}}>
+                    Country tracker
+                </Typography>
 
+                <Box>
+                    {navItems.map((item) => (
+                        <Button key={item.name} onClick={() => handleNavigation(item.path)}>{item.name}</Button>
+                    ))}
+                </Box>
             </Toolbar>
         </AppBar>
 
