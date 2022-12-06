fec73fa36e092a5d700b8f9bfbf445381e21df93
fangfangwang <fannie-id@hotmail.com>
2022-12-06 22:20:31 +0100

    Add visitedCountryList.

--- frontend/src/component/WorldMap.tsx
+++ frontend/src/component/WorldMap.tsx
@@ -1,25 +1,23 @@
 import React, {useState} from "react";
-import { ComposableMap, Geographies, Geography } from 'react-simple-maps';
+import {ComposableMap, Geographies, Geography} from 'react-simple-maps';
 import {Country} from "../model/Country";
 import {User} from "../model/User"
 import "./WorldMap.css";
 
 
-
 type WorldMapProps = {
-    loggedInUser:User,
+    loggedInUser: User,
     countries: Country[]
-
 }
 
-const visited:Country []=[]
-const visitedId:string []=[]
+const visited: Country [] = []
+
 
 const geoUrl =
     "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
-export default function WorldMap(props:WorldMapProps) {
+export default function WorldMap(props: WorldMapProps) {
 
-    const[selectedCountries,setSelectedCountries] = useState<string[]>([])
+    const [selectedCountries, setSelectedCountries] = useState<string[]>([])
 
 
     const handleClick = (event: any) => {
@@ -27,34 +25,34 @@ export default function WorldMap(props:WorldMapProps) {
         const countryID = event
 
 
-
         for (const country of countries) {
             if (countryID === country.threeLetterCode) {
                 console.log(country)
                 visited.push(country)
-                visitedId.push(countryID)
-                setSelectedCountries(visitedId)
-
+                setSelectedCountries(prevState => [...prevState, countryID])
             }
         }
-        console.log("event",event)
-        //console.log(countryID)
-        console.log("visitedId: ",visitedId)
         return visited
     }
 
     return (<ComposableMap>
-                <Geographies geography={geoUrl}>
-                    {({geographies}) =>
-                        geographies.map((geo) => (
-                            <Geography
-                                fill = {selectedCountries.includes(geo.id) ? "#78002e" : "#f8bbd0"}
-                                onClick={() => {
+            <Geographies geography={geoUrl}>
+                {({geographies}) =>
+                    geographies.map((geo) => (
+                        <Geography
+                            fill={selectedCountries.includes(geo.id) ? "#78002e" : "#f8bbd0"}
+                            style={{
+                                hover: {
+                                    fill: "#ad1457",
+                                    outline: "none"
+                                },
+                            }}
+                            onClick={() => {
                                 handleClick(`${geo.id}`)
                             }} key={geo.rsmKey} geography={geo}/>
-                        ))
-                    }
-                </Geographies>
-            </ComposableMap>
-        )
-    }
+                    ))
+                }
+            </Geographies>
+        </ComposableMap>
+    )
+}
