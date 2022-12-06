51158fbf1485cecbe3058449b2e2fb536b2805d4
nick <klockgether.nick@gmail.com>
2022-12-06 09:04:48 +0100

    Update TravellerControllerTest to use DirtiesContext

--- backend/src/test/java/de/neuefische/backend/controller/TravellerControllerTest.java
+++ backend/src/test/java/de/neuefische/backend/controller/TravellerControllerTest.java
@@ -9,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -22,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
 class TravellerControllerTest {
 
     String travellerEndPoint = "/api/traveller";
