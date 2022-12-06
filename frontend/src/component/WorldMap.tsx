cfbf0a34c9a465ebfc5b2f8c1a3a70728f317923
nick <klockgether.nick@gmail.com>
2022-12-06 22:52:01 +0100

    Update WorldMap to add countries to user, and colorize countries by Remove now redundant variables

--- frontend/src/component/WorldMap.tsx
+++ frontend/src/component/WorldMap.tsx
@@ -1,8 +1,9 @@
-import React, {useState} from "react";
+import React, { useEffect, useState} from "react";
 import {ComposableMap, Geographies, Geography} from 'react-simple-maps';
 import {Country} from "../model/Country";
 import {User} from "../model/User"
 import "./WorldMap.css";
+import {updateUser} from "../apiCalls";
 
 
 type WorldMapProps = {
@@ -10,27 +11,36 @@ type WorldMapProps = {
     countries: Country[]
 }
 
-const visited: Country [] = []
-
-
 const geoUrl =
     "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
 export default function WorldMap(props: WorldMapProps) {
 
     const [selectedCountries, setSelectedCountries] = useState<string[]>([])
 
+    useEffect(() => {
 
-    const handleClick = (countryID: string) => {
-        const countries = props.countries
+        props.loggedInUser.visitedCountries.forEach((visitedCountry) => {
+            setSelectedCountries(prevState => [...prevState, visitedCountry.threeLetterCode])
+        })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
 
-        for (const country of countries) {
-            if (countryID === country.threeLetterCode) {
-                console.log(country)
-                visited.push(country)
-                setSelectedCountries(prevState => [...prevState, countryID])
+
+    const handleCountryClick = (countryId: string) => {
+        const countryByCountryId = props.countries.find((country) => {
+            if (country.threeLetterCode === countryId) {
+                return country
+            }else{
+                return undefined
             }
+        });
+
+        if (countryByCountryId) {
+            props.loggedInUser.visitedCountries.push(countryByCountryId)
+            setSelectedCountries(prevState => [...prevState, countryByCountryId.threeLetterCode])
+            updateUser(props.loggedInUser.id, props.loggedInUser)
+                .catch(console.error)
         }
-        return visited
     }
 
     return (<ComposableMap>
@@ -40,13 +50,16 @@ export default function WorldMap(props: WorldMapProps) {
                         <Geography
                             fill={selectedCountries.includes(geo.id) ? "#78002e" : "#f8bbd0"}
                             style={{
+                                default: {
+                                    outline: "black"
+                                },
                                 hover: {
                                     fill: "#ad1457",
                                     outline: "none"
                                 },
                             }}
                             onClick={() => {
-                                handleClick(`${geo.id}`)
+                                handleCountryClick(`${geo.id}`)
                             }} key={geo.rsmKey} geography={geo}/>
                     ))
                 }
