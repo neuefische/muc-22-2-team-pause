00d2635cf6594ef130bd73bf6c2dee1d388091cc
nick <klockgether.nick@gmail.com>
2022-12-07 15:58:35 +0100

    Add mui components to Login

--- frontend/src/component/Login.tsx
+++ frontend/src/component/Login.tsx
@@ -1,13 +1,17 @@
 import {ChangeEvent, FormEvent, useState} from "react";
 import {UserLoginRequest} from "../model/User";
 import {useNavigate} from "react-router-dom";
-type LoginProps ={
-    handleLogInUser(user:UserLoginRequest):Promise<string>
+import {Box, IconButton, TextField, Typography} from "@mui/material";
+import {LoginSharp} from "@mui/icons-material";
+
+type LoginProps = {
+    handleLogInUser(user: UserLoginRequest): Promise<string>
 }
-export default function Login(props:LoginProps) {
+export default function Login(props: LoginProps) {
     const [username, setUsername] = useState("")
     const [password, setPassword] = useState("")
     const navigate = useNavigate();
+
     function handleUsernameChange(event: ChangeEvent<HTMLInputElement>) {
         setUsername(event.target.value)
     }
@@ -19,18 +23,24 @@ export default function Login(props:LoginProps) {
 
     function handleSubmit(event: FormEvent<HTMLFormElement>) {
         event.preventDefault();
-        props.handleLogInUser({username:username,password:password}).then(() => {
+        props.handleLogInUser({username: username, password: password}).then(() => {
             navigate("/overview")
         })
     }
 
-    return (<div>
-            <h2>Login Page</h2>
+    return (<Box flexDirection={"column"}
+                 display={"grid"}
+                 flexWrap={"wrap"}
+                 justifyContent={"center"}
+                 margin={"30px"}>
+            <Typography variant={"h4"} textAlign={"center"} sx={{mb:5}}>Login</Typography>
             <form onSubmit={handleSubmit}>
-                <input type={"text"} placeholder={"username"} value={username} onChange={handleUsernameChange}/>
-                <input type={"password"} placeholder={"password"} value={password} onChange={handlePasswordChange}/>
-                <button type={"submit"}> Login</button>
+                <TextField onChange={handleUsernameChange} value={username} placeholder={"Hi, traveller"}/>
+                <TextField type={"password"} onChange={handlePasswordChange} value={password} placeholder={"****"}/>
+                <IconButton type={"submit"} color={"secondary"}>
+                    <LoginSharp/>
+                </IconButton>
             </form>
-        </div>
+        </Box>
     )
 }
