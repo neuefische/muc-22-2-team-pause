775acbda9dafe142f7e2e5134d41631bbdd183e5
fangfangwang <fannie-id@hotmail.com>
2022-12-07 21:06:33 +0100

    Change ui @ WelcomeScreen.tsx

--- frontend/src/component/WelcomeScreen.tsx
+++ frontend/src/component/WelcomeScreen.tsx
@@ -1,4 +1,6 @@
 import './Welcome.css';
+import {Box, IconButton} from "@mui/material";
+import {AddSharp, LoginSharp} from "@mui/icons-material";
 
 export default function WelcomeScreen() {
     return (
@@ -11,12 +13,22 @@ export default function WelcomeScreen() {
                 </p>
 
                 <ul className="content__container__list">
-                    <li className="content__container__list__item">world !</li>
+                    <li className="content__container__list__item">world</li>
 
-                    <li className="content__container__list__item">travellers !</li>
+                    <li className="content__container__list__item">travellers</li>
 
                 </ul>
             </div>
+            <Box sx={{mt:4}}>
+                <IconButton sx={{ml:14}} size= "large"  color={"secondary"} type={"submit"}>
+                    <AddSharp />
+                </IconButton>
+
+                <IconButton size= "large" type={"submit"} color={"secondary"}>
+                    <LoginSharp />
+                </IconButton>
+            </Box>
+
 
         </div>)
 }
