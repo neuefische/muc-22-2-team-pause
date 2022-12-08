0e2653fad3cff47274ac284172ac371751475765
fangfangwang <fannie-id@hotmail.com>
2022-12-08 15:58:08 +0100

    Add link in IconBtn

--- frontend/src/component/WelcomeScreen.tsx
+++ frontend/src/component/WelcomeScreen.tsx
@@ -1,11 +1,10 @@
 import './Welcome.css';
 import {Box, IconButton} from "@mui/material";
 import {AddSharp, LoginSharp} from "@mui/icons-material";
+import {Link} from "react-router-dom";
 
 export default function WelcomeScreen() {
     return (
-
-
         <div className="content">
             <div className="content__container">
                 <p className="content__container__text">
@@ -14,21 +13,19 @@ export default function WelcomeScreen() {
 
                 <ul className="content__container__list">
                     <li className="content__container__list__item">world</li>
-
                     <li className="content__container__list__item">travellers</li>
 
                 </ul>
             </div>
-            <Box sx={{mt:4}}>
-                <IconButton sx={{ml:14}} size= "large"  color={"secondary"} type={"submit"}>
-                    <AddSharp />
+            <Box sx={{mt: 4}}>
+                <IconButton sx={{ml: 14}} size="large" color={"secondary"} component={Link} to="/signup">
+                    <AddSharp/>
                 </IconButton>
 
-                <IconButton size= "large" type={"submit"} color={"secondary"}>
-                    <LoginSharp />
+                <IconButton sx={{ml: 3}} size="large" color={"secondary"} component={Link} to="/login">
+                    <LoginSharp/>
                 </IconButton>
             </Box>
-
 
         </div>)
 }
