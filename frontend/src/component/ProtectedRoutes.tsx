9869b79ae33ac7d4cd128d9f39848d81ad9441e2
nick <klockgether.nick@gmail.com>
2022-12-09 16:26:59 +0100

    ProtectedRoutes.tsx get now properly rendered

--- frontend/src/component/ProtectedRoutes.tsx
+++ frontend/src/component/ProtectedRoutes.tsx
@@ -1,24 +1,13 @@
 import {Navigate, Outlet} from "react-router-dom";
 import {Traveller} from "../model/User";
-import {getLoggedInTravellerByLoggedInUser} from "../apiCalls";
-import {useEffect, useState} from "react";
 
 type ProtectedRoutesProps = {
     loggedInTraveller: Traveller;
 }
 
 export default function ProtectedRoutes(props: ProtectedRoutesProps) {
-    const [isAuthenticated, setIsAuthenticated] = useState<boolean>(false)
 
-    useEffect(()=>{
-        getLoggedInTravellerByLoggedInUser().then(data => {
-            console.log(data)
-            setIsAuthenticated(true)
-        }).catch(() => {
-            setIsAuthenticated(false)
-        })
-    },[])
-
+    const  isAuthenticated = props.loggedInTraveller.id !== "";
     return (
         {isAuthenticated} ? <Outlet/> : <Navigate to={"/login"}/>
     )
