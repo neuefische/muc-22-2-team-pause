89d20304bae3e944efed4a72803317d36ec77f02
nick <klockgether.nick@gmail.com>
2022-12-11 10:47:10 +0100

    Add tooltip for countries on the map

--- frontend/src/component/WorldMap.tsx
+++ frontend/src/component/WorldMap.tsx
@@ -3,22 +3,22 @@ import {ComposableMap, Geographies, Geography} from 'react-simple-maps';
 import {Country} from "../model/Country";
 import {Traveller} from "../model/User"
 import {updateUser} from "../apiCalls";
+import {Box, Tooltip, Typography} from "@mui/material";
 
 
 type WorldMapProps = {
     loggedInTraveller: Traveller,
-    countries:Country[]
+    countries: Country[]
 }
 
 const geoUrl =
     "https://raw.githubusercontent.com/deldersveld/topojson/master/world-countries.json"
 export default function WorldMap(props: WorldMapProps) {
-
-    const [countries,setCountries] = useState<string[]>([])
+    const [visitedCountries, setVisitedCountries] = useState<string[]>([])
     useEffect(() => {
 
-        props.loggedInTraveller.visitedCountries.forEach((country:Country) => {
-            setCountries(prevState => [...prevState, country.threeLetterCode])
+        props.loggedInTraveller.visitedCountries.forEach((country: Country) => {
+            setVisitedCountries(prevState => [...prevState, country.threeLetterCode])
         })
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
@@ -35,34 +35,49 @@ export default function WorldMap(props: WorldMapProps) {
 
         if (countryById) {
             props.loggedInTraveller.visitedCountries.push(countryById)
-            setCountries(prevState => [...prevState, countryById.threeLetterCode])
+            setVisitedCountries(prevState => [...prevState, countryById.threeLetterCode])
             updateUser(props.loggedInTraveller.id, props.loggedInTraveller)
                 .catch(console.error)
         }
     }
 
-    return (<ComposableMap>
-            <Geographies geography={geoUrl}>
-                {({geographies}) =>
-                    geographies.map((geo) => (
-                        <Geography
-                            fill={countries.includes(geo.id) ? "#78002e" : "#f8bbd0"}
-                            style={{
-                                default: {
-                                    outline: "black"
-                                },
-                                hover: {
-                                    fill: "#ad1457",
-                                    outline: "none"
-                                },
-                            }}
-                            onClick={() => {
-                                handleClick(`${geo.id}`)
-                            }} key={geo.rsmKey} geography={geo}/>
-                    ))
-                }
-            </Geographies>
-        </ComposableMap>
+    return (
+        <Box>
+            <Typography variant={"h4"} textAlign={"center"} sx={{mt:3}}>Personal Map</Typography>
+
+            <Typography variant={"subtitle2"}
+                        textAlign={"center"}
+                        sx={{mt:3}}>Click on a country to add it to your list
+            </Typography>
+
+            <Box>
+                <ComposableMap>
+                    <Geographies geography={geoUrl}>
+                        {({geographies}) =>
+                            geographies.map((geo) => (
+                                <Tooltip key={"tooltip:"+geo.rsmKey} title={geo.properties.name}>
+                                <Geography
+                                    fill={visitedCountries.includes(geo.id) ? "#78002e" : "#f8bbd0"}
+                                    style={{
+                                        default: {
+                                            outline: "black"
+                                        },
+                                        hover: {
+                                            fill: "#ad1457",
+                                            outline: "none"
+                                        },
+                                    }}
+                                    onClick={() => {
+                                        handleClick(`${geo.id}`)
+                                    }} key={geo.rsmKey} geography={geo}/>
+                                </Tooltip>
+                            ))
+                        }
+                    </Geographies>
+                </ComposableMap>
+            </Box>
+        </Box>
+
     )
 }
 
