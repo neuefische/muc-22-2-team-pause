2ab4fa721c44cba7031b75e0592e09f283b8e620
nick <klockgether.nick@gmail.com>
2022-12-11 10:05:29 +0100

    Add one more condition to ProtectedRoutes.tsx to make sure not anonymousUser gets detected

--- frontend/src/component/ProtectedRoutes.tsx
+++ frontend/src/component/ProtectedRoutes.tsx
@@ -3,11 +3,13 @@ import {Traveller} from "../model/User";
 
 type ProtectedRoutesProps = {
     loggedInTraveller: Traveller;
+    username: string
 }
 
 export default function ProtectedRoutes(props: ProtectedRoutesProps) {
 
-    const  isAuthenticated = props.loggedInTraveller.id !== "";
+    const isAuthenticated = props.loggedInTraveller.id !== "" &&
+        (props.username !== "" && props.username !== "anonymousUser")
 
     return (
         isAuthenticated ? <Outlet/> : <Navigate to={"/login"}/>
